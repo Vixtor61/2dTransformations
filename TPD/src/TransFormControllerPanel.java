18ea9eb3ef3950795c255fc078fee9bed29f977d
Vixtor61 <00198117@uca.edu.sv>
2021-06-03 17:43:33 -0600

    GUI improvements

--- TPD/src/TransFormControllerPanel.java
+++ TPD/src/TransFormControllerPanel.java
@@ -1,19 +1,35 @@
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class TransFormControllerPanel extends JPanel {
 	SuperDuperButton addLevel;
 	SuperDuperButton subLevel;
 	DrawPanel drawPanel;
 	MFrame.OpLevel transLevel;
+	JLabel levelLabel; //create a label
+	
+	
+
+
+
+
 
 	public TransFormControllerPanel(DrawPanel drawPanel, MFrame.OpLevel oplevel) {
-		this.setLayout(new GridLayout(1, 3));
+		this.setLayout(new GridLayout(2, 1));
+		JPanel layoutPanel = new JPanel();
+		layoutPanel.setLayout(new GridLayout(1, 3));
 		this.drawPanel = drawPanel;
 		this.transLevel = oplevel;
 
+		JLabel label = new JLabel(oplevel.Op,SwingConstants.CENTER); //create a label
+	
+		levelLabel = new JLabel(""+oplevel.L,SwingConstants.CENTER);
+	
+		
 		addLevel = new SuperDuperButton("+");
 		addLevel.addActionListener(e -> {
 			btnOperation(oplevel.Op, true);
@@ -22,14 +38,21 @@ public class TransFormControllerPanel extends JPanel {
 		subLevel.addActionListener(e -> {
 			btnOperation(oplevel.Op, false);
 		});
-
-		this.add(addLevel);
-		this.add(subLevel);
+		
+		layoutPanel.add(addLevel);
+		layoutPanel.add(levelLabel);
+		layoutPanel.add(subLevel);
+		this.add(label);
+		this.add(layoutPanel);
+		
+		
 	}
 
 	public void btnOperation(String option, boolean op) {
-		modifyLevel(transLevel,op);
+		
 		transLevel.modifyLevel(op);
+		levelLabel.setText(""+transLevel.L);
+
 		drawPanel.sqr.transFormSquare(transLevel.returnLevelVal(), option);
 		drawPanel.sqr.printCords();
 		drawPanel.repaint();
